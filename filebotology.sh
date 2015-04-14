ad39737d45753d7aff4896911abd41dbd131c4e5
Fabio Calefato <calefato.uniba@gmail.com>
2015-04-14 19:08:32 +0200

    rationalized structure

--- filebotology.sh
+++ filebotology.sh
@@ -26,35 +26,12 @@ print_help() {
         printf "Help documentation $SCRIPT\n\n"
         printf "Basic usage:\n"
         printf "Command line switches are mandatory. The following switches are recognized.\n"
-        printf "\t\t -t type \t --Sets the type of media to scan. Allowed values are 'tv' or 'movie'.\n"
-        printf "\t\t -p path \t --Sets the path where to look for media. No default value is set.\n"
-        printf "\t\t -h \t\t --Displays this help message. No further functions are performed.\n\n"
+        printf "\t -t type \t --Sets the type of media to scan. Allowed values are 'tv' or 'movie'.\n"
+        printf "\t -p path \t --Sets the path where to look for media. No default value is set.\n"
+        printf "\t -h \t\t --Displays this help message. No further functions are performed.\n\n"
         printf "Example: $SCRIPT -t tv -p /volume1/video/tvshows\n"
         exit 1
 }
-                                                                                                
-
-#Check the number of arguments. If none are passed, print help and exit.
-NUMARGS=$#
-if [ $NUMARGS -lt 2 ]; then
-	printf "Wrong number of arguments, provided $NUMARGS resquested 2.\n\n"
-	print_help;
-fi
-
-# parse args
-while getopts "t:p:h" FLAG; do
-	case $FLAG in
-		t) MEDIATYPE=$OPTARG;;
-		p) MEDIAPATH=$OPTARG;;
-		h) print_help;;
-		\?) #unrecognized option - show help
-	            printf "Use $SCRIPT -h to see the help documentation.\n"
-		    exit 2;;
-        esac
-done
-shift $((OPTIND-1))  #This tells getopts to move on to the next argument.
-### end getopts code ###
-
 
 # get new or missing subs
 get_missing_subs() {
@@ -77,9 +54,28 @@ rename_subs_in_path() {
 	printf "\n------------------------\n" >> $LOG
 }
 
+#Check the number of arguments. If none are passed, print help and exit.
+NUMARGS=$#
+if [ $NUMARGS -lt 2 ]; then
+	printf "Wrong number of arguments, provided $NUMARGS resquested 2.\n\n"
+	print_help;
+fi
+
+# parse args
+while getopts "t:p:h" FLAG; do
+	case $FLAG in
+		t) MEDIATYPE=$OPTARG;;
+		p) MEDIAPATH=$OPTARG;;
+		h) print_help;;
+		\?) #unrecognized option - show help
+	            printf "Use $SCRIPT -h to see the help documentation.\n"
+		    exit 2;;
+        esac
+done
+shift $((OPTIND-1))  #This tells getopts to move on to the next argument.
+### end getopts code ###
 
 ### main loop to process files ###
-
 get_missing_subs $MEDIATYPE $MEDIAPATH
 rename_subs_in_path $MEDIATYPE $MEDIAPATH
 
