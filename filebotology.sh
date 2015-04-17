b06014a51dfde5cca46157ef1a59a3b41fc34c05
Fabio Calefato <calefato.uniba@gmail.com>
2015-04-17 16:12:16 +0200

    fixed issue #5
    add control of mandatory args and options

--- filebotology.sh
+++ filebotology.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
 ##
-# TODO: 	add control of mandatory args
-#
 # Author: 	bateman
 # Date: 	Jan. 28, 2015
 # Rev:		Apr. 17, 2015
-# Ver:		0.7
+# Ver:		1.0
 ## 
 
 #Set Script Name variable
@@ -32,13 +30,12 @@ VERBOSE="off"
 # print help instructions
 print_help() {
         printf "Help documentation $SCRIPT\n\n"
-        printf "Basic usage:\n"
-        printf "Command line switches are mandatory. The following switches are recognized.\n"
-        printf "\t -t type \t -- Sets the type of media to scan. Allowed values are 'tv' or 'movie'.\n"
-        printf "\t -p path \t -- Sets the path where to look for media. No default value is set.\n"
-	printf "\t -l lang \t -- Sets the two-letter code for subs language (default is 'en').\n\n"
-	printf "\t -r lang \t -- Renames subs replacing 3-letter code with 2-letter one (e.g, from 'eng' to 'en'). Must match -l arg.\n\n"
-	printf "\t -v \t\t -- Enables verbose output on the console, disabled by default.\n\n"
+        printf "The following command line options are recognized, -t and -p are mandatory.\n"
+        printf "\t -t type \t -- Mandatory, sets the type of media to scan. Allowed values are 'tv' or 'movie'.\n"
+        printf "\t -p path \t -- Mandatory, sets the path where to look for media.\n"
+	printf "\t -l lang \t -- Sets the two-letter code for subs language (default arg is 'en').\n"
+	printf "\t -r lang \t -- Renames subs replacing 3-letter code with 2-letter one (e.g, from 'eng' to 'en'). Must match -l arg.\n"
+	printf "\t -v \t\t -- Enables verbose output on the console, disabled by default.\n"
         printf "\t -h \t\t -- Displays this help message. No further functions are performed.\n\n"
         printf "Example: $SCRIPT -t tv -p /volume1/video/tvshows\n"
         exit 1
@@ -77,8 +74,8 @@ rename_subs_in_path() {
 #Check the number of arguments. If none are passed, print help and exit.
 NUMARGS=$#
 if [ $NUMARGS -lt 2 ]; then
-	printf "Wrong number of arguments, provided $NUMARGS, resquested 2.\n"
-	printf "Please, note that -p and -l arguments are mandatory. \n\n"
+	printf "Wrong number of arguments, provided $NUMARGS, resquested 2.\n" >&2
+	printf "Please, note that -p and -l arguments are mandatory. \n\n" >&2
 	print_help;
 fi
 
@@ -93,16 +90,21 @@ while getopts "t:p:l:r:vh" FLAG; do
 		    printf "Entering verbose mode, messages will appear in both console and log file.\n";;
 		h ) print_help;;
 		\?) #unrecognized option - show help
-	            printf "Use $SCRIPT -h to see the help documentation.\n"
+	            printf "Use $SCRIPT -h to see the help documentation.\n" >&2
 		    exit 2;;
-		: ) printf "Missing option argument for -$OPTARG" 
+		: ) printf "Missing option argument for -$OPTARG" >&2
 		    exit 2;;
-        	* ) printf "Unimplemented option: -$OPTARG"
+        	* ) printf "Unimplemented option: -$OPTARG" >&2
         	    exit 2;;
         esac
 done
 shift $((OPTIND-1))  #This tells getopts to move on to the next argument.
 ### end getopts code ###
+
+if [ "${MEDIAPATH}" == "" || "${MEDIATYPE}" == "" ]; then
+    echo "-t or -p options are missing" >&2
+    print_help
+fi
 
 ### main instruction set to process files ###
 exec 3>&1 1>>${LOG} 2>&1 # redirects stdout and stderr to the log file, binds fd 3 to stdout
