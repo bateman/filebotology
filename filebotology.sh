98fc6384902ea83a87010c9a1437422cbe3ff178
root <fcalefato@gmail.com>
2015-06-13 20:31:56 +0200

    added '| tee -a ' to filebot call to append outpÃut to the LOG file

--- filebotology.sh
+++ filebotology.sh
@@ -3,8 +3,8 @@
 ##
 # Author: 	bateman
 # Date: 	Jan. 28, 2015
-# Rev:		Apr. 19, 2015
-# Ver:		1.0
+# Rev:		Jun. 13, 2015
+# Ver:		1.1
 ## 
 
 #Set Script Name variable
@@ -58,7 +58,7 @@ get_missing_subs() {
 		DB="--db TheMovieDB"
 	fi
 	print "--- Start finding missing subtitles in $LANG2 from $MEDIAPATH at $(date +"%Y-%m-%d %H-%M-%S"). ---"
-	filebot -script fn:suball -get-missing-subtitles $DB --lang $LANG2 --format $FORMAT $MEDIAPATH $VERB_CMD
+	filebot -script fn:suball -get-missing-subtitles $DB --lang $LANG2 --format $FORMAT $MEDIAPATH $VERB_CMD | tee -a $LOG
 	print "--- Done with missing subs at $(date +"%Y-%m-%d %H-%M-%S"). ---"
 }
 
@@ -66,7 +66,7 @@ get_missing_subs() {
 rename_subs_in_path() {
 	if [ "${LANG3}" != "" ]; then
 		print "---- Start renaming subtitles from $LANG3 to $LANG2 in $MEDIAPATH at $(date +"%Y-%m-%d %H-%M-%S"). ---"
-		filebot -r -script fn:replace --def "e=.$LANG3.srt" "r=.$LANG2.srt" $MEDIAPATH $VERB_CMD
+		filebot -r -script fn:replace --def "e=.$LANG3.srt" "r=.$LANG2.srt" $MEDIAPATH $VERB_CMD | tee -a $LOG
 		print "---- Done with renaming subs at $(date +"%Y-%m-%d %H-%M-%S"). ---" 
 	fi
 }
