4f1426ab2a54ee8209ca804eda77d0f054c389fa
root <fcalefato@gmail.com>
2015-06-14 10:04:21 +0200

    renamed, addedd new typeface and colors

---
+++ colorsformat.inc.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+##
+# Author:       bateman
+# Date:         Jan. 28, 2015
+# Rev:          Jun. 14, 2015
+# Ver:          1.2
+##
+
+#Set colors as ENV VARS for console output 
+
+# mapped onto ERROR (bold)
+RED='\e[1;91m'
+# mapped onto INFO (bold)
+CYAN='\e[1;96m'
+# mapped onto NOTICE (bold)
+GREEN='\e[1;92m'
+# used in print_help()
+BOLD='\e[1m'
+UNDER='\e[4m'
+# unused 
+BLUE='\e[94m'
+YELLOW='\e[93m'
+PURPLE='\e[95m'
+DIM='\e[2m'
+# reset to No Color
+NC='\e[0m'
+
