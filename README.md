4073a0a1f5ea70c7da0cb88dcca882d5e804729c
Fabio Calefato <fcalefato@gmail.com>
2015-08-04 12:41:24 +0200

    fixed issue #15

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # filebotology
-An ash script (yes, Synology boxes run ash shell not bash through Busybox) to autosearch for subtitles on a NAS. 
+An ash script (yes, Synology boxes run ash shell through Busybox, not bash) to autosearch for subtitles on a NAS. 
 This script relies on [Filebot cli] (http://www.filebot.net/cli.html) capabilities to automate the search of subtitles for your videos stored in your beloved Synology NAS.
 
 ## Requirements
@@ -11,36 +11,58 @@ This script relies on [Filebot cli] (http://www.filebot.net/cli.html) capabiliti
   2. Install _Java 8 SE Embedded_ for ARM architecture. Note that, as of this writing, ver. 8 is not supported as an official package from Synology through the _Package center_ -- the Java Manager is infact stuck at ver. 7. Therefore, you need to add a custom Package Source: go to Package Center ► Settings ► Package Sources ► Add ► Name: SynologyItalia and Location:  [http://spk.synologyitalia.com/] (http://spk.synologyitalia.com/) ► OK. After that, Java 8 should show up with other extra packages in the Community list. (*Make sure to pick the one that's right for your CPU model!* See [here] (http://forum.synology.com/wiki/index.php/What_kind_of_CPU_does_my_NAS_have) if you don't know already.)
   2. Add Package Source also for _Filebot_; go to Package Center ► Settings ► Package Sources ► Add ► Name: FileBot and Location: [https://packages.filebot.net/syno/] (https://packages.filebot.net/syno/) ► OK. Now find and install it from Community packages list.
   3. Also from the community packages, select and install _Git_ to ease the download of the script itself.
-  4. Got to [www.opensubtitles.org] (www.opensubtitles.org) and sign up; now open an SSH session, and run the following from command line:
-   * `filebot -script fn:configure` and enter your credentials
-   * if you forget, you'll get this error message in the log/console as a reminder: `CmdlineException: OpenSubtitles: Please enter your login details by calling "filebot -script fn:configure"`
+  4. Got to [www.opensubtitles.org] (www.opensubtitles.org), sign up and write down your credentials: they should either be hardcoded in the script, or passed as argument to the command line options, see next section (CLI execution) for more.
   5. Still via SSH, checkout the script in your NAS:
    * `$ cd /volume1/git/`
    * `$ git clone https://github.com/bateman/filebotology.git`
+   * Then, make sure the script are executable on your system:
+   * `$ chmod +x *.sh`
   6. Go to the Control Panel ► Task Scheduler (i.e., the DSM cron equivalent) and create one or two tasks for TV Shows and/or Movies, accordingly:
-   * `$ /volume1/git/filebotology/filebotology.sh -t tv -p /volume1/video/tvshows/ >> /var/log/filebotology.log`
-   * `$ /volume1/git/filebotology/filebotology.sh -t movies -p /volume1/video/movies/ >> /var/log/filebotology.log`
-   * Choose how often they run; make sure to not run them at the same minute, so their execution won't overlap; use root as task owner.
+   * `$ /volume1/git/filebotology/filebotology.sh -u username -s secret -t tv -p /volume1/video/tvshows/`
+   * `$ /volume1/git/filebotology/filebotology.sh -u username -s secret -t movies -p /volume1/video/movies/`
+   * Make them run once per day (no more often than that, or you risk to be banned); make sure to not run them at the same hour:minute, so their execution won't overlap; use root as task owner.
   7. Install the logrotate config for rotating the script log (_change the logrotate options as you wish_):
    * `$ cd /etc/logrotate.d`
    * `$ ln -s /volume1/git/filebotology/fbt-logrotate filebotology`
    * to check the status do a `$ cat /var/lib/logrotate.status | grep filebot`, it will return something like this: `"/var/log/filebotology.log" 2015-3-21-12:0:0`
+  8. If you want to enable error notification via email, you should properly configure ssmtp on your NAS. To do so, edit these file as follows:
+   * Edit file `/etc/ssmtp/ssmtp.conf` and paste the following snippet, changing text as needed:
+   ```
+   root=YOUR_GMAIL_USERNAME@gmail.com
+   mailhub=smtp.gmail.com:587
+   hostname=YOUR_NAS_BOX_NAME
+   UseTLS=YES
+   UseSTARTTLS=YES
+   AuthUser=YOUR_GMAIL_USERNAME (without @gmail.com)
+   AuthPass=YOUR_APP-SPECIFIC_PASSWORD (you can generate one here: https://security.google.com/settings/security/apppasswords)
+   FromLineOverride=YES
+   ```
+   * Now edit this other file `/etc/ssmtp/revaliases` (usually empty), adding this line:
+   `root:YOUR_GMAIL_USERNAME@gmail.com:smtp.gmail.com:587`
+   * Test that everything is working, executing from comman line: 
+   `echo "ssmtp test" | ssmtp YOUR_GMAIL_USERNAME@gmail.com`
 
 ## CLI execution
 To run it from the command line, excute the script with the `-h` flag to print the following help menu. Please, note that the first two options `-t` and `-p` are **mandatory**.
 ```
--t type  Sets the type of media to scan. Allowed values are 'tv' and 'movie'.
--p path  Sets the path where to look for media. No default value is set.
--l lang  Sets the two-letter code for subs language (ISO 639-1, see http://goo.gl/KXQ0x7). Default is 'en'.
--r lang  Renames subs from the three-letter code (ISO 639-2) to the two-letter one. Must match -l arg.
--v       Enables verbose output on the console, disabled by default.
--h       Displays this help message. No further functions are performed.
+-t type     Mandatory, sets the type of media to scan. Allowed values are 'tv' and 'movie'.
+-p path     Mandatory, sets the path where to look for media. No default value is set.
+-u username Mandatory, sets the OpenSubtitles.org username for authenticating
+-s secret   Mandatory, sets the OpenSubtitles.org secret (password) for authenticating
+-l lang     Sets the two-letter code for subs language (ISO 639-1, see http://goo.gl/KXQ0x7). Default is 'en'.
+-r lang     Renames subs from the three-letter code (ISO 639-2) to the two-letter one. Must match -l arg.
+-e email    Sets the recipient address for enabling the notification of errors by email
+-v          Enables verbose output on the console, disabled by default.
+-h          Displays this help message. No further functions are performed.
 ```
 
-Example: `$ filebotology.sh -v -l it -r ita -t tv -p /volume1/video/tvshows`
+Example: `$ filebotology.sh -v -u username -s secret -l it -r ita -t tv -p /volume1/video/tvshows`
 
 Now, sit back and relax, it's gonna take a long while if you have a large base of videos!
 
 ## Notes
+* As of ver. 1.3, entering [OpenSubitles.org] (www.opensubtitles.org) credentials has been implemented as mandatory since authenticated users have a much higher per-day limit for downloading subtitles (currently 200). This is useful, especially on first runs, where a whole lot of subs might be found. You might consider a donation of 10$/€ to become a [VIP member] (http://www.opensubtitles.org/en/support#vip) and further increase this limit. To check your daily quota, execute: `filebot -script fn:osdb.stats`
+* If you don't want to supply as command line arguments your email address and your [OpenSubitles.org] (www.opensubtitles.org) credentials, you can manually edit the script, entering hard-coded values at the beginning of the file `filebotology.sh` where vars `USERNAME`, `PASSWORD` and `EMAIL` are declared as empty.
 * At the moment the script is set to download subtitles only in `srt` format from [OpenSubitles.org] (www.opensubtitles.org), using hash-based matching. 
 * If you are some kind of a *nix shell hacker and find some quirks in the script, keep in mind that the ash shell implementation of Busybox is somewhat limited, so not all functions normally found in a full-fledged shell are available here.
+* Colors and fonts can be customized editing file `colorsformat.inc.sh`. See here for more options: http://misc.flogisoft.com/bash/tip_colors_and_formatting. Please, note that `dim` and `blink` options are not supported by Synology ash shell.
--- filebotology.sh
+++ filebotology.sh
@@ -3,8 +3,8 @@
 ##
 # Author: 	bateman
 # Date: 	Jan. 28, 2015
-# Rev:		Apr. 19, 2015
-# Ver:		1.0
+# Rev:		Aug. 4, 2015
+# Ver:		1.3beta
 ## 
 
 #Set Script Name variable
@@ -25,28 +25,49 @@ FORMAT="srt"
 LOG="/var/log/filebotology.log"
 # verbose switch, default off
 VERBOSE="off"
+# opensubtitles.org username and password, may be hardcoded or supplied as opt arguments
+USERNAME=""
+PASSWORD=""
+# email address to send error notification to
+EMAIL=""
+
+# loads color env vars for stdout colors
+source ./colorsformat.inc.sh
 
 
 # print help instructions
 print_help() {
-		printf "Help documentation $SCRIPT\n\n"
-		printf "The following command line options are recognized, -t and -p are mandatory.\n"
-		printf "\t -t type \t -- Mandatory, sets the type of media to scan. Allowed values are 'tv' or 'movie'.\n"
-		printf "\t -p path \t -- Mandatory, sets the path where to look for media.\n"
-		printf "\t -l lang \t -- Sets the two-letter code for subs language (default arg is 'en').\n"
-		printf "\t -r lang \t -- Renames subs replacing 3-letter code with 2-letter one (e.g, from 'eng' to 'en'). Must match -l arg.\n"
-		printf "\t -v \t\t -- Enables verbose output on the console, disabled by default.\n"
-		printf "\t -h \t\t -- Displays this help message. No further functions are performed.\n\n"
-		printf "Example: $SCRIPT -t tv -p /volume1/video/tvshows\n"
+		printf "\nHelp documentation for ${BOLD}$SCRIPT ${NC}\n\n"
+		printf "The following command line options are recognized, ${UNDER}-t and -p are mandatory${NC}.\n"
+		printf "\t ${BOLD}-t type ${NC}\t -- ${UNDER}Mandatory${NC}, sets the type of media to scan. Allowed values are 'tv' or 'movie'.\n"
+		printf "\t ${BOLD}-p path ${NC}\t -- ${UNDER}Mandatory${NC}, sets the path where to look for media.\n"
+		printf "\t ${BOLD}-u username ${NC}\t -- ${UNDER}Mandatory${NC}, sets the OpenSubtitles.org username for authenticating.\n"
+		printf "\t ${BOLD}-s secret ${NC}\t -- ${UNDER}Mandatory${NC}, sets the OpenSubtitles.org secret (password) for authenticating.\n"
+		printf "\t ${BOLD}-l lang ${NC}\t -- Sets the two-letter code for subs language (default arg is 'en').\n"
+		printf "\t ${BOLD}-r lang ${NC}\t -- Renames subs replacing 3-letter code with 2-letter one (e.g, from 'eng' to 'en'). Must match -l arg.\n"
+		printf "\t ${BOLD}-v ${NC}\t\t -- Enables verbose output on the console, disabled by default.\n"
+		printf "\t ${BOLD}-e ${NC}\t\t -- Sets the recipient address for enabling the notification of errors by email.\n"
+		printf "\t ${BOLD}-h ${NC}\t\t -- Displays this help message. No further functions are performed.\n\n"
+		printf "Example: ${BOLD}$SCRIPT -t tv -p /volume1/video/tvshows ${NC}\n\n"
 		exit 1
 }
 
 # redefine an echo function depending on verbose switch 
 print() {
+	local level=$1
+        local code=$GREEN # default is "NOTICE"
+        if [ "${level}" = 'ERROR' ]; then
+		code=$RED
+	elif [ "${level}" = 'INFO' ]; then
+		code=$CYAN
+	fi
+	
 	if [ "${VERBOSE}" == 'on' ]; then
-		echo $1 | tee -a $LOG
+		printf "${code}[${level}]${NC}: " | tee -a $LOG
+		echo $2 | tee -a $LOG
 	else
-		echo $1 
+		printf "${code}[${level}]${NC}: "
+		echo $2 
 	fi
 }
 
@@ -57,52 +78,106 @@ get_missing_subs() {
 	elif [ "${MEDIATYPE}" == 'movie' ]; then
 		DB="--db TheMovieDB"
 	fi
-	print "--- Start finding missing subtitles in $LANG2 from $MEDIAPATH at $(date +"%Y-%m-%d %H-%M-%S"). ---"
-	filebot -script fn:suball -get-missing-subtitles $DB --lang $LANG2 --format $FORMAT $MEDIAPATH $VERB_CMD
-	print "--- Done with missing subs at $(date +"%Y-%m-%d %H-%M-%S"). ---"
+	
+	print "NOTICE" "Start finding missing subtitles in $LANG2 from $MEDIAPATH at $(date +"%Y-%m-%d %H-%M-%S")."
+	filebot -script fn:suball -get-missing-subtitles $DB --lang $LANG2 --format $FORMAT $MEDIAPATH $VERB_CMD | tee -a $LOG
+	if [ "$?" == 0 ]; then
+		print "NOTICE" "Done with finding missing subs at $(date +"%Y-%m-%d %H-%M-%S")."
+	else
+	        print "ERROR" "Something went wrong, filebot error code $?"
+	fi
 }
 
 # rename to chosen format
 rename_subs_in_path() {
 	if [ "${LANG3}" != "" ]; then
-		print "---- Start renaming subtitles from $LANG3 to $LANG2 in $MEDIAPATH at $(date +"%Y-%m-%d %H-%M-%S"). ---"
-		filebot -r -script fn:replace --def "e=.$LANG3.srt" "r=.$LANG2.srt" $MEDIAPATH $VERB_CMD
-		print "---- Done with renaming subs at $(date +"%Y-%m-%d %H-%M-%S"). ---" 
+		print "NOTICE" "Start renaming subtitles from $LANG3 to $LANG2 in $MEDIAPATH at $(date +"%Y-%m-%d %H-%M-%S")."
+		filebot -r -script fn:replace --def "e=.$LANG3.srt" "r=.$LANG2.srt" $MEDIAPATH $VERB_CMD | tee -a $LOG
+		if [ "$?" == 0 ]; then
+			print "NOTICE" "Done with renaming subs at $(date +"%Y-%m-%d %H-%M-%S")."
+		else
+			print "ERROR" "Something went wrong, filebot error code $?"
+		fi 
 	fi
 }
 
-#Check the number of arguments. At least -t and -p must be passed (2), with their arguments (2)
-#Otherwise, print help and exit
-NUMARGS=$#
-if [ $NUMARGS -lt 4 ]; then
-	printf "\nERROR: Wrong number of arguments, provided $((NUMARGS / 2)), requested at least 2.\n\n" >&2
-	print_help
-fi
+# checks that at least mandatory opt args have been provided correctly
+check_args_consistency() {
+	if [ "${MEDIATYPE}" == "" ]; then
+		print "ERROR" "-t option argument is missing."
+		print_help
+	elif [ "${MEDIAPATH}" == "" ]; then
+		print "ERROR" "-p option argument is missing."
+		print_help
+	fi
+}
+
+# checks that opensubtitles credentials have been provided and are working
+check_osdb_credentials() {
+	if [ "${USERNAME}" == "" ] || [ "${PASSWORD}" == "" ]; then
+		print "ERROR" "OpenSubtitle.org credentials have not been provided. Please register here: http://www.opensubtitles.org/en/newuser"
+		exit 2
+	fi
+	# now check credentials are working
+	print "NOTICE" "Logging in OpenSubtitles.org..."
+	echo -e "${USERNAME}\n""${PASSWORD}\n" | filebot -script fn:configure > /dev/null
+	if [ "$?" == 0 ]; then
+		print "NOTICE" "Authentication OK!"
+	elif [ "$?" == 1 ]; then
+		print "ERROR" "401 Unauthorized: Please check your credentials, error code $?"
+		if [ "${EMAIL}" != "" ]; then
+			print "ERROR" "Sending error notification by email"
+			send_email "401 Unauthorized: Please check your credentials, error code $?"
+		fi
+		exit 2
+	elif [ "$?" > 1 ]; then
+		print "ERROR" "Something went wrong, filebot error code $?"
+ 		if [ "${EMAIL}" != "" ]; then
+ 			print "ERROR" "Sending error notification by email"
+			send_email "Something went wrong, filebot error code $?"
+		fi
+		exit 2
+	fi        
+}
+
+# send email via ssmtp
+send_email() {
+    local emailfile="/tmp/fb-email.txt"
+    local body=$1
+    echo "To: $EMAIL" > $emailfile
+    echo "From: filebotology@noreply" >> $emailfile
+    echo "Subject: filebotology error notification" >> $emailfile
+    echo -e "\n$body" >> $emailfile
+    ssmtp $EMAIL < $emailfile
+}
 
 # parse args
-while getopts "t:p:l:r:vh" FLAG; do
+while getopts "t:p:u:s:l:r:e:vh" FLAG; do
 	case $FLAG in
 		t ) MEDIATYPE=$OPTARG
-			if [ "${MEDIATYPE}" != "tv" ] && [ "${MEDIATYPE}" != "movie" ]; then 
-				echo "\nERROR: -t option either is missing or has wrong argument.\n\n" >&2
+			if [ "${MEDIATYPE}" != 'tv' ] && [ "${MEDIATYPE}" != 'movie' ]; then 
+				print "ERROR" "-t option has wrong argument." #>&2
 				print_help
 			fi;;
 		p ) MEDIAPATH=$OPTARG
-			if [ "${MEDIAPATH}" == "" ]; then
-				echo "\nERROR: -p option argument is missing.\n\n" >&2
-				print_help
+			if [ ! -d "${MEDIAPATH}" ]; then
+				print "ERROR" "Directory ${MEDIAPATH} does not exist."
+				exit 2
 			fi;;
+		u ) USERNAME=$OPTARG;;
+		s ) PASSWORD=$OPTARG;;
+		e ) EMAIL=$OPTARG;; 
 		l ) LANG2=$(echo "$OPTARG" | tr '[A-Z]' '[a-z]');; # to lower case
 		r ) LANG3=$(echo "$OPTARG" | tr '[A-Z]' '[a-z]');; # to lower case
 		v ) VERBOSE='on'
-			printf "Entering verbose mode, messages will appear in both console and log file.\n";;
+			print "INFO" "Entering verbose mode, messages will appear in both console and log file.";;
 		h ) print_help;;
 		\?) #unrecognized option - show help
-			printf "Use $SCRIPT -h to see the help documentation.\n" >&2
+			printf "INFO" "Use $SCRIPT -h to see the help documentation." 
 			exit 2;;
-		: ) printf "Missing option argument for -$OPTARG" >&2
+		: ) print "ERROR" "Missing option argument for -$OPTARG"
 			exit 2;;
-		* ) printf "Unimplemented option: -$OPTARG" >&2
+		* ) printf "ERROR" "Unimplemented option: -$OPTARG" 
 			exit 2;;
 		esac
 done
@@ -115,8 +190,11 @@ if [ $VERBOSE = "on" ]; then
 else
 	exec 2>&1 1>>$LOG # redirects stderr to stdout, both to LOG file
 fi
-get_missing_subs $MEDIATYPE $MEDIAPATH
-rename_subs_in_path $MEDIATYPE $MEDIAPATH
+
+check_args_consistency
+check_osdb_credentials
+get_missing_subs
+rename_subs_in_path
 ### end main ###
 
 exit 0
